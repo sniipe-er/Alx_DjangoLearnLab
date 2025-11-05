9dfc6d20e865d61811885600e70f7aad3e3f89b8
sniipe-er <ss.w.rengard@gmail.com>
2025-11-05 19:31:41 +0100

    .

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,6 @@ from models import *
 
 books = Author.objects.get(name = author_name)
 books = Book.objects.filter(author = author)
-books = books.all()
 library = Library.objects.get(name=library_name)
 Librarian = Librarian.objects.get(library = library_name)
 Library = Librarian.library
