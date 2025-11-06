d45ad5f65e3a98e1b467dc724b4f8da81c00486c
sniipe-er <ss.w.rengard@gmail.com>
2025-11-06 13:29:08 +0100

    .

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail_view'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'),name='logout'),
-    path('register/', view.register(template_name='register.html'),name='register')
+    path('register/', views.register(template_name='register.html'),name='register')
 ]
