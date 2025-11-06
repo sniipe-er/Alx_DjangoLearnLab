2e837a2b09d273c499ef2c6f54fcf231e1fc5acc
sniipe-er <ss.w.rengard@gmail.com>
2025-11-06 23:32:14 +0100

    .

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,6 +8,12 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField()
-    author = models.ForeignKey(Author)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     def __str__(self):
         return self.title
+    
+class Library(models.Model):
+    name = models.CharField()
+    books = models.ManyToManyField(Book, related_name='library')
+    def __str__(self):
+        return self.name
