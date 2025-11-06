6ad7243b2d89518438e1aefa201450fd55ac5407
sniipe-er <ss.w.rengard@gmail.com>
2025-11-06 23:09:18 +0100

    .

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,6 +8,6 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField()
-    Author = models.ForeignKey(Author)
+    author = models.ForeignKey(Author)
     def __str__(self):
         return self.title
