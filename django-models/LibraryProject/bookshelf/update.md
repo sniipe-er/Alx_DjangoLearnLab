2c2eebe1800ee1b9131fd04390de543b4a256f5b
sniipe-er <ss.w.rengard@gmail.com>
2025-11-06 21:32:28 +0100

    .

--- django-models/LibraryProject/bookshelf/update.md
+++
@@ -1,4 +0,0 @@
-book = Book.objects.get(title = "1984")
-book.title = "Nineteen Eighty-Four"
-book.save()
-#updated successfully
--- django-models/LibraryProject/relationship_app/query_samples.py
+++
@@ -1,8 +0,0 @@
-from .models import *
-
-author = Author.objects.get(name=author_name)
-books = Book.objects.filter(author=author)
-books = books.all()
-library = Library.objects.get(name=library_name)
-librarian = Librarian.objects.get(library=library)
-library_from_librarian = librarian.library
