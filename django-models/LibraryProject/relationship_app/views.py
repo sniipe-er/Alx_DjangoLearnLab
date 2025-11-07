39c9e6f5cd19a0eb93fe02991c6629baf3902212
sniipe-er <ss.w.rengard@gmail.com>
2025-11-07 11:24:48 +0100

    .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Library
 from .models import *
+from django.views.generic.detail import DetailView
+
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
