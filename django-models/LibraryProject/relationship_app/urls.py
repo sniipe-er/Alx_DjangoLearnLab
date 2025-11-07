a6b42bb07fe53e58b8b6dd90b93c494904d8ef1f
sniipe-er <ss.w.rengard@gmail.com>
2025-11-07 19:49:53 +0100

    .

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import list_books, LibraryDetailView
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('librarydetail/<int:pk>/', LibraryDetailView.as_view(), name='LibraryDetailView'),
+]
