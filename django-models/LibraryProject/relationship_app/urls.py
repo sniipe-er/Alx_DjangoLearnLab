0f26c0841b936b735adfeef2be90309a3fa425be
sniipe-er <ss.w.rengard@gmail.com>
2025-11-07 11:36:00 +0100

    .

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,5 +4,5 @@ from .views import list_books
 
 urlpatterns = [
     path('list_books', views.list_books, name='list_books'),
-    path('library_detail', views.library_detail, name='library_detail')
+    path('library_detail', views.library_detail.as_view(), name='library_detail')
 ]
