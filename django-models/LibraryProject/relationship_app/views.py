41a50ba3633f30b5fcbfb33e62108af0201cd5a2
sniipe-er <ss.w.rengard@gmail.com>
2025-11-07 11:16:14 +0100

    .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    context = {'book_list': books}
+    return render(request, 'book/book_list.html', context)
