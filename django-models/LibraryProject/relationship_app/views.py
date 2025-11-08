3b695def2c080ab32ce5d37d46cb40d29806e1b6
sniipe-er <ss.w.rengard@gmail.com>
2025-11-08 13:41:17 +0100

    .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,4 +20,4 @@ class LibraryDetailView(ListView):
 class SignUpView(CreateView):
     form_class = UserCreationForm()
     success_url = reverse_lazy("login")
-    template_name = "register.html"
+    template_name = "relationship_app/register.html"
