6a5b9e76500dbc8f8879e1a5d2b8f297dd98ae29
sniipe-er <ss.w.rengard@gmail.com>
2025-11-09 23:44:47 +0100

    .

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,30 +1,38 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
     def __str__(self):
         return self.title
     
 class Library(models.Model):
-    name = models.CharField()
-    books = models.ManyToManyField(Book, related_name='library')
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
     def __str__(self):
         return self.name
-
+    
 class Librarian(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
     
-    class UserProfile(models.Model):
+class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
