8725a1fce977db92ac39258beba29d496987ed74
sniipe-er <ss.w.rengard@gmail.com>
2025-11-18 13:05:04 +0100

    .

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Book (models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publication_year = models.IntegerField()
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField()
