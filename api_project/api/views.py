dcd5f44b03bf8a19cf2dff20f15f72f5a079b367
sniipe-er <ss.w.rengard@gmail.com>
2025-11-23 23:39:48 +0100

    .

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BBookViewSet(generics.ModelViewSet):
+class BookViewSet(generics.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
