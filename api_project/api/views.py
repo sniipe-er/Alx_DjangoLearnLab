54df18ac7f7a6cedabf0ff4e1812724c385142d0
sniipe-er <ss.w.rengard@gmail.com>
2025-11-23 23:25:58 +0100

    .

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView
 # Create your views here.
+class Booklist (ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
