160b6e0b2528956f3814b8cf92c000563a1e5b15
sniipe-er <ss.w.rengard@gmail.com>
2025-11-23 23:40:31 +0100

    .

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,12 @@
-from rest_framework import generics
+from django.shortcuts import render
+from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 from .models import Book
-from rest_framework import viewsets
-from rest_framework.viewsets import ModelViewSet
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(generics.ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
