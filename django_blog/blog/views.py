01ecd91ec3afd82f6084b28939cdc6dd1cfa6e7e
sniipe-er <ss.w.rengard@gmail.com>
2025-12-06 23:10:47 +0100

    .

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, ProfileUpdateForm
+
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "blog/register.html", {"form": form})
+
+
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+    return render(request, "blog/profile.html", {"form": form})
