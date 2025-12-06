68a971b3fa55ad14603e93584eecbc0eefe05291
sniipe-er <ss.w.rengard@gmail.com>
2025-12-06 13:24:18 +0100

    .

---
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+class Book(models.Model):
+    title = models.CharField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
