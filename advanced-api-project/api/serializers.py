322bb0bff0d332fe7c7d96deba49ee81806f4880
sniipe-er <ss.w.rengard@gmail.com>
2025-12-06 13:36:42 +0100

    .

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -12,6 +12,7 @@ class BookSerializer(serializers.ModelSerializer):
         return value    
 
 class AuthorSerializer(serializers.ModelSerializer):
+     books = BookSerializer(many=True, read_only=True)
     class Meta:
         model = Author
         fields = ['id', 'name']
