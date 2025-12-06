ccebe4e3c4c7f49979c60f83d3eb06ad89e45a68
sniipe-er <ss.w.rengard@gmail.com>
2025-12-06 13:35:45 +0100

    .

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -5,6 +5,11 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'publication_year']
+    
+    def validate_publication_year(self, value):
+        if value > 2025:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value    
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
