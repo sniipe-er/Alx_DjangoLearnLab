b0ef0504a305108f5424e521c95dbc59b6805161
sniipe-er <ss.w.rengard@gmail.com>
2025-12-14 13:32:07 +0100

    .

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     # Comments
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-add'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-add'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
